15d1dfcc38494e8267e1e916ae362e69ac3be4a0
phattdh2007@gmail.com <tdhphat337>
2025-10-01 07:54:39 +0700

    ppp

--- src/CoolComputer.tsx
+++ src/CoolComputer.tsx
@@ -1,10 +1,11 @@
-import { motion, useMotionValue, useSpring } from 'framer-motion';
-import { useContext, useEffect, useRef, useState } from 'react';
+import { motion, useSpring } from 'framer-motion';
+import { useContext, useEffect, useRef } from 'react';
 import { MyCt } from './Navigator';
 
 const CoolComputer = () => {
     const legacy = useContext(MyCt)!;
     const box = useRef <HTMLDivElement> (null);
+    const boxes = Array(3).fill(useRef <HTMLDivElement> (null));
 
     const x = useSpring(0, {
         stiffness: 200,
@@ -16,21 +17,34 @@ const CoolComputer = () => {
         // console.log(legacy.ID, legacy.prev);
     }, [legacy.ID]);
 
+    useEffect(() => {
+        const handle = (e: MouseEvent): void => {
+            if (box.current!.contains(e.target as Node)) {
+                // something process to hidden and visible navigator, i'm so lazy =))
+            }
+        }
+
+        window.addEventListener('mousemove', handle);
+
+        return () => window.removeEventListener('mousemove', handle);
+    }, []);
+
     return (
         <div className='w-[450px] h-[250px] flex justify-around items-center'> 
             <div className='cursor-pointer'
                 onClick={() => legacy.setId(0)}
             >{'<'}</div>
 
-            <div className='w-[400px] h-[230px] bg-black overflow-hidden' ref={box}>
-                <motion.div className='w-[80%] h-[100%] flex justify-center items-center bg-black relative '
+            <div className='w-[320px] h-[230px] flex justify-center items-center overflow-hidden' ref={box} >
+                <motion.div className='w-[100%] h-[100%] flex justify-center items-center relative '
                     style={{ x }}
                 >
                     {legacy.Img.map((img, id) => (
                         <img key={id} src={img} alt=""  className='absolute'
                             style={{
-                                left: 430 * id + 38,//`${id * 300}`,
+                                left: 430 * id,//`${id * 300}`,
                             }}
+                            ref={boxes[id]} 
                         />
                     ))}
                 </motion.div>
